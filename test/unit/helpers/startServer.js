8edb5b47973019a62b422e27e682dafe4d7b254d
nick-holvast1 <nick.holvast@engineering.digital.dwp.gov.uk>
2022-08-01 20:22:24 +0100

    test: updated startServer test to include error condition

--- test/unit/helpers/startServer.js
+++ test/unit/helpers/startServer.js
@@ -1,15 +1,22 @@
 const initStartServer = require('../../../src/helpers/startServer')
-
 const sandbox = sinon.createSandbox()
 
 describe('helpers/startServer', () => {
   let appMock,
+    consoleMock,
+    errorStub,
+    exitSpy,
     listenStub,
-    startServer
+    logStub,
+    startServer,
+    testErrorState
 
   const host = 'nickholvast.com'
   const port = 3000000
   const startMsg = 'Windows 95 start sound goes here'
+  const logMsg = `${startMsg} ${host}:${port}`
+
+  const error = new Error('Goat in the server')
 
   beforeEach(() => {
     // stub app.listen() function
@@ -17,17 +24,44 @@ describe('helpers/startServer', () => {
     listenStub.withArgs(port, host).returns(undefined)
     listenStub.returns(Error('app.listen called with unexpected parameters'))
 
+    // stub logging
+    logStub = sandbox.stub()
+    logStub.withArgs(logMsg).returns(undefined)
+    logStub.returns(Error('console.log called with unexpected parameters'))
+    errorStub = sandbox.stub()
+    errorStub.withArgs(error).returns(undefined)
+    errorStub.returns(Error('console.error called with unexpected parameters'))
+
     // mocks express app
     appMock = {
       listen: listenStub
     }
 
+    // mocks console
+    consoleMock = {
+      error: errorStub,
+      log: logStub
+    }
+
+    // spy on process.exit
+    exitSpy = sandbox.spy()
+
+    // default to start server with no error
     startServer = initStartServer({
       app: appMock,
+      exit: exitSpy,
       host,
+      logger: consoleMock,
       port,
       startMsg
     })
+
+    // set testErrorState to true in error state tests
+    if (testErrorState) {
+      listenStub
+        .withArgs(port, host)
+        .throws(error)
+    }
 
     startServer()
   })
@@ -41,6 +75,52 @@ describe('helpers/startServer', () => {
       assert.isTrue(
         listenStub.calledOnceWith(port, host),
         'app.listen not called with expected parameters'
+      )
+    })
+    it('should log the start message and not log an error', () => {
+      assert.isTrue(
+        logStub.calledOnceWith(logMsg),
+        'console.log not called once with expected log message'
+      )
+
+      assert.isTrue(errorStub.notCalled)
+    })
+    it('should not exit', () => {
+      assert.isTrue(
+        exitSpy.notCalled,
+        'process.exit() called when not expected'
+      )
+    })
+  })
+
+  describe('server start with error', () => {
+    it('should handle an error when the server fails to start successfully', () => {
+      // overrides listenStub to throw error
+      testErrorState = true
+
+      assert.isTrue(
+        listenStub.calledOnceWith(port, host),
+        'app.listen not called with expected parameters'
+      )
+    })
+
+    it('should call console.error and not call console.log', () => {
+      testErrorState = true
+
+      assert.isTrue(logStub.notCalled)
+
+      assert.isTrue(
+        errorStub.calledOnceWith(error),
+        'logger.error not called once with expected error'
+      )
+    })
+
+    it('should call process.exit with code \'1\'', () => {
+      testErrorState = true
+
+      assert.isTrue(
+        exitSpy.calledOnceWith(1),
+        'process.exit() not called when expected'
       )
     })
   })
