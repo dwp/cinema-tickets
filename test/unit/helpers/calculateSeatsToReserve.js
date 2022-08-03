38b26fee4233b8d51d8a198f5697e8c1e659dee1
nick-holvast1 <nick.holvast@engineering.digital.dwp.gov.uk>
2022-08-03 17:02:26 +0100

    test: tdd tests for helper/calculateSeatsToReserve

---
+++ test/unit/helpers/calculateSeatsToReserve.js
@@ -0,0 +1,17 @@
+import initCalculateSeatsToReserve from '../../../src/helpers/calculateSeatsToReserve.js'
+import C from '../../../src/constants/index.js'
+
+describe('helpers/calculateSeatsToReserve', () => {
+  const calculateSeatsToReserve = initCalculateSeatsToReserve({ C })
+  
+  it('should reserve seats for a valid request', () => {
+    const tickets = { adult: 5, child: 5, infant: 3 }
+    const expectedResult = 10
+    const result = calculateSeatsToReserve({ tickets })
+    assert.equal(
+      expectedResult,
+      result,
+      'Unexpected number of seats allocated'
+    )
+  })
+})
