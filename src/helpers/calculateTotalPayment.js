2aebfeb058ea9b648119e4b570d3b545d2a2c218
nick-holvast1 <nick.holvast@engineering.digital.dwp.gov.uk>
2022-08-03 10:29:02 +0100

    test: unit tests for calculateTotalPayment helper + skeleton src code

---
+++ src/helpers/calculateTotalPayment.js
@@ -0,0 +1,7 @@
+export default ({
+  C
+}) => ({
+  tickets
+}) => (
+  0
+)
---
+++ test/unit/helpers/calculateTotalPayment.js
@@ -0,0 +1,86 @@
+import initCalculateTotalPayment from '../../../src/helpers/calculateTotalPayment.js'
+
+describe('helpers/calculateTotalPayment', () => {
+  const C = {
+    tickets: {
+      type: {
+        adult: {
+          price: 20,
+        },
+        child: {
+          price: 10,
+        },
+        infant: {
+          price: 0,
+        }
+      }
+    }
+  }
+
+  const calculateTotalPayment = initCalculateTotalPayment({ C })
+
+  describe('invalid requests', () => {
+    it('should reject an empty request', () => {
+      const error = new Error('Invalid ticket request')
+      const tickets = {}
+      assert.throws(
+        calculateTotalPayment({ tickets }),
+        error,
+        'Expected error not thrown'
+        )
+      })
+    })
+
+  describe('valid requests', () => {
+    it('should calculate requests for only adults', () => {
+      const tickets = {
+        adult: 5,
+        child: 0,
+        infant: 0
+      }
+      const totalPayment = calculateTotalPayment({ tickets })
+      const expectedPayment = C.tickets.type.adult.price * tickets.adult
+      assert.equal(
+        expectedPayment,
+        totalPayment,
+        'Total payment does not equal expected payment'
+      )
+    })
+    
+    it('should calculate requests for adults and children', () => {
+      const tickets = {
+        adult: 1,
+        child: 19,
+        infant: 0
+      }
+      const totalPayment = calculateTotalPayment({ tickets })
+      const expectedPayment = (
+        (C.tickets.type.adult.price * tickets.adult)
+        + (C.tickets.type.child.price * tickets.child)
+      )
+      assert.equal(
+        expectedPayment,
+        totalPayment,
+        'Total payment does not equal expected payment'
+      )
+    })
+    
+    it('should calculate requests for adults, children and infants', () => {
+      const tickets = {
+        adult: 10,
+        child: 10,
+        infant: 1000
+      }
+      const totalPayment = calculateTotalPayment({ tickets })
+      const expectedPayment = (
+        (C.tickets.type.adult.price * tickets.adult)
+        + (C.tickets.type.child.price * tickets.child)
+      )
+      assert.equal(
+        expectedPayment,
+        totalPayment,
+        'Total payment does not equal expected payment'
+      )
+    })
+  })
+})
