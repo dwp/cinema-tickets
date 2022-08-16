8ef2b58a6348d6726d90a1c56baf39ce6009d7b7
Lewis Jones <lewis.jones@engineering.digital.dwp.gov.uk>
2022-08-16 17:07:19 +0100

    feat: use dependency injection for testing mocked dependencies and implement call to payment service

--- cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
+++ cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
@@ -1,16 +1,18 @@
 package uk.gov.dwp.uc.pairtest;
 
+import thirdparty.paymentgateway.TicketPaymentService;
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
 import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
 
 public class TicketServiceImpl implements TicketService {
-    /**
-     * Should only have private methods other than the one below.
-     */
+    private TicketPaymentService ticketPaymentService;
+
+    public TicketServiceImpl(TicketPaymentService ticketPaymentService) {
+        this.ticketPaymentService = ticketPaymentService;
+    }
 
     @Override
     public void purchaseTickets(Long accountId, TicketTypeRequest... ticketTypeRequests) throws InvalidPurchaseException {
-
+        ticketPaymentService.makePayment(accountId, 20);
     }
-
 }
