9dc126e61adcd722a31e7f82aaf0db0c9406af6c
Lewis Jones <lewis.jones@engineering.digital.dwp.gov.uk>
2022-08-16 17:14:56 +0100

    feat: children cannot order tickets without at least one adult

--- cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
+++ cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
@@ -2,6 +2,7 @@ package uk.gov.dwp.uc.pairtest;
 
 import thirdparty.paymentgateway.TicketPaymentService;
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
+import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest.Type;
 import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
 
 public class TicketServiceImpl implements TicketService {
@@ -13,6 +14,20 @@ public class TicketServiceImpl implements TicketService {
 
     @Override
     public void purchaseTickets(Long accountId, TicketTypeRequest... ticketTypeRequests) throws InvalidPurchaseException {
+        int numberOfAdultTickets = 0;
+        int numberOfChildTickets = 0;
+
+        for (TicketTypeRequest ticketTypeRequest : ticketTypeRequests) {
+            if (ticketTypeRequest.getTicketType() == Type.ADULT) {
+                numberOfAdultTickets += ticketTypeRequest.getNoOfTickets();
+            } else if (ticketTypeRequest.getTicketType() == Type.CHILD) {
+                numberOfChildTickets += ticketTypeRequest.getNoOfTickets();
+            }
+        }
+
+        if (numberOfChildTickets != 0 && numberOfAdultTickets == 0) {
+            throw new InvalidPurchaseException("ERROR: At least one adult ticket is required when purchasing a child/infant ticket");
+        }
         ticketPaymentService.makePayment(accountId, 20);
     }
 }
