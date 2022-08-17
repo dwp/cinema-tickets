6d4c4b0645f4458a28cf38ba4a957ea2396f7a19
Lewis Jones <lewis.jones@engineering.digital.dwp.gov.uk>
2022-08-17 18:03:42 +0100

    feat: add simple logging to purchaseTickets method

--- cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
+++ cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
@@ -1,6 +1,8 @@
 package uk.gov.dwp.uc.pairtest;
 
 import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import thirdparty.paymentgateway.TicketPaymentService;
 import thirdparty.seatbooking.SeatReservationService;
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
@@ -12,6 +14,7 @@ public class TicketServiceImpl implements TicketService {
 
   private final TicketPaymentService ticketPaymentService;
   private final SeatReservationService seatReservationService;
+  private static final Logger LOGGER = Logger.getLogger( TicketServiceImpl.class.getName() );
 
   public TicketServiceImpl(TicketPaymentService ticketPaymentService,
       SeatReservationService seatReservationService) {
@@ -25,9 +28,11 @@ public class TicketServiceImpl implements TicketService {
     validateRequest(accountId, ticketTypeRequests);
 
     int totalPaymentAmount = calculateTotalPaymentAmount(ticketTypeRequests);
+    LOGGER.log(Level.INFO, String.format("Making request to TicketPaymentService to take payment of £%s", totalPaymentAmount));
     ticketPaymentService.makePayment(accountId, totalPaymentAmount);
 
     int numberOfSeatsToReserve = calculateTotalNumberOfSeatsToReserve(ticketTypeRequests);
+    LOGGER.log(Level.INFO, String.format("Making request to SeatReservationService to reserve %s seat(s)",numberOfSeatsToReserve));
     seatReservationService.reserveSeat(accountId, numberOfSeatsToReserve);
   }
 
