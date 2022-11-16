599376eca2ed820f7979156715e901e7db9e08ca
Will L <william.lawrence7@icloud.com>
2022-11-16 13:34:17 +0000

    Sets up environment for lint and test

---
+++ cinema-tickets-javascript/test/TicketService.spec.js
@@ -0,0 +1,13 @@
+import TicketService from '../src/pairtest/TicketService';
+
+describe('TicketService tests', () => {
+  let testTicketService;
+
+  beforeEach(() => {
+    testTicketService = new TicketService();
+  });
+
+  it('should...', () => {
+    expect('foo').toBeTruthy();
+  });
+});
