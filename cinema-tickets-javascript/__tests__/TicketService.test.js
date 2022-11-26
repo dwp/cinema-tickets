e8a777b1e4851e6a339a58cfa085e0789b027df6
Hiroji <joegilbertdev@outlook.com>
2022-11-26 16:00:19 +0000

    setup- enabled jest to use ES6 module import by configuring Babel

---
+++ cinema-tickets-javascript/__tests__/TicketService.test.js
@@ -0,0 +1,9 @@
+import TicketService from "../src/pairtest/TicketService"
+
+describe("", () => {
+    test("", () => {
+        const actual =  new TicketService;
+        console.log(actual.purchaseTickets(1, []));
+        
+    })
+})
--- cinema-tickets-javascript/src/pairtest/TicketService.js
+++ cinema-tickets-javascript/src/pairtest/TicketService.js
@@ -1,6 +1,21 @@
 import TicketTypeRequest from './lib/TicketTypeRequest.js';
 import InvalidPurchaseException from './lib/InvalidPurchaseException.js';
 
+/*
+Provide a working implementation of a `TicketService` that:
+- Considers the above objective, business rules, constraints & assumptions.
+
+- Calculates the correct amount for the requested tickets and makes a payment request to the `TicketPaymentService`.  
+- must be perfect request as tickets are purchased
+
+- Calculates the correct no of seats to reserve and makes a seat reservation request to the `SeatReservationService`.
+- must be perfect request as seats are booked  
+
+- Rejects any invalid ticket purchase requests. It is up to you to identify what should be deemed as an invalid purchase request.
+
+//ticketTypeRequest ARG = object {ADULT: NUM, CHILD, NUM, INFANT: NUM}
+*/
+
 export default class TicketService {
   /**
    * Should only have private methods other than the one below.
@@ -8,5 +23,15 @@ export default class TicketService {
 
   purchaseTickets(accountId, ...ticketTypeRequests) {
     // throws InvalidPurchaseException
+
+    /*Plan:
+    Should check inputs using private methods and then make a request to 
+    - TicketPaymentService
+    - SatReservtionService
+    */
   }
+
+  
 }
+
+
