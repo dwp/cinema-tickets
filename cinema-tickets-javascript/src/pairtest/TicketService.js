2bdf0bddb615f829b8a98bb45c1f53a9430bab23
Tom Baxter <thomas99baxter@gmail.com>
2022-11-28 20:23:39 +0000

    style(lib): auto fix linting errors

--- cinema-tickets-javascript/src/pairtest/TicketService.js
+++ cinema-tickets-javascript/src/pairtest/TicketService.js
@@ -1,12 +1,12 @@
-import TicketTypeRequest from './lib/TicketTypeRequest.js';
-import InvalidPurchaseException from './lib/InvalidPurchaseException.js';
+import TicketTypeRequest from './lib/TicketTypeRequest.js'
+import InvalidPurchaseException from './lib/InvalidPurchaseException.js'
 
 export default class TicketService {
   /**
    * Should only have private methods other than the one below.
    */
 
-  purchaseTickets(accountId, ...ticketTypeRequests) {
+  purchaseTickets (accountId, ...ticketTypeRequests) {
     // throws InvalidPurchaseException
   }
 }
--- cinema-tickets-javascript/src/pairtest/lib/TicketTypeRequest.js
+++ cinema-tickets-javascript/src/pairtest/lib/TicketTypeRequest.js
@@ -3,30 +3,30 @@
  */
 
 export default class TicketTypeRequest {
-  #type;
+  #type
 
-  #noOfTickets;
+  #noOfTickets
 
-  constructor(type, noOfTickets) {
+  constructor (type, noOfTickets) {
     if (!this.#Type.includes(type)) {
-      throw new TypeError(`type must be ${this.#Type.slice(0, -1).join(', ')}, or ${this.#Type.slice(-1)}`);
+      throw new TypeError(`type must be ${this.#Type.slice(0, -1).join(', ')}, or ${this.#Type.slice(-1)}`)
     }
 
     if (!Number.isInteger(noOfTickets)) {
-      throw new TypeError('noOfTickets must be an integer');
+      throw new TypeError('noOfTickets must be an integer')
     }
 
-    this.#type = type;
-    this.#noOfTickets = noOfTickets;
+    this.#type = type
+    this.#noOfTickets = noOfTickets
   }
 
-  getNoOfTickets() {
-    return this.#noOfTickets;
+  getNoOfTickets () {
+    return this.#noOfTickets
   }
 
-  getTicketType() {
-    return this.#type;
+  getTicketType () {
+    return this.#type
   }
 
-  #Type = ['ADULT', 'CHILD', 'INFANT'];
+  #Type = ['ADULT', 'CHILD', 'INFANT']
 }
