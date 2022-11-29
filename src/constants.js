d49f137590a082cdd6bfa79764241c298c033783
Tom Baxter <thomas99baxter@gmail.com>
2022-11-29 15:06:01 +0000

    chore: move price list and ticket types to constants file

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+const PERMITTED_TICKET_TYPES = Object.freeze['ADULT', 'CHILD', 'INFANT']
+
+const PRICE_LIST = Object.freeze({
+  ADULT: 20,
+  CHILD: 10,
+  INFANT: 0
+})
+
+export { PERMITTED_TICKET_TYPES, PRICE_LIST }
