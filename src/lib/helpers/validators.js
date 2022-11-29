13bdc6feb5e737c20a1c77122eb5078c0cc4d8e8
Tom Baxter <thomas99baxter@gmail.com>
2022-11-29 14:31:42 +0000

    feat: validate against invalid account numbers

--- src/lib/helpers/validators.js
+++ src/lib/helpers/validators.js
@@ -1,5 +1,12 @@
 export function accountIDValidator (accountID) {
     const errors = [];
 
+    if(accountID !== 0 && !accountID) {
+        errors.push('AccountID is not provided!')
+    } else if (typeof accountID !== 'number') {
+        errors.push('AccountID must be a number')
+    } else if (accountID === 0) {
+        errors.push('AccountID cannot be zero!')
+    }
     return errors;
 }
--- test/lib/helpers/validators.spec.js
+++ test/lib/helpers/validators.spec.js
@@ -5,8 +5,23 @@ import { accountIDValidator } from '../../../src/lib/helpers/validators.js'
 describe('validators', () => {
    describe('accountIDValidator', () => {
         it('should return no errors for a valid accountID', () => {
-            const result = accountIDValidator(1);
-            expect(result).to.be.deep.eq([]);
+            expect(accountIDValidator(1)).to.be.deep.eq([]);
         });
+
+        it('should return an error when AccountID is null', () => {
+            expect(accountIDValidator(null)).to.be.deep.eq(['AccountID is not provided!'])
+        });
+
+        it('should return an error when AccountID is undefined', () => {
+            expect(accountIDValidator(undefined)).to.be.deep.eq(['AccountID is not provided!'])
+        })
+
+        it('should return an error when AccountID is not a number', () => {
+            expect(accountIDValidator('13')).to.be.deep.eq(['AccountID must be a number'])
+        })
+
+        it('should return an error when AccountID is zero', () => {
+            expect(accountIDValidator(0)).to.be.deep.eq(['AccountID cannot be zero!'])
+        })
    }); 
 });
