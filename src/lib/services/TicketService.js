d334ef66ca4def0cab7a1654526134aae38d4bd7
Tom Baxter <thomas99baxter@gmail.com>
2022-11-29 14:47:49 +0000

    feat: add accountID validation to TicketService

--- src/lib/services/TicketService.js
+++ src/lib/services/TicketService.js
@@ -1,6 +1,7 @@
 import TicketTypeRequest from '../TicketTypeRequest.js'
 import InvalidPurchaseException from '../InvalidPurchaseException.js'
 import TicketPaymentService from '../../thirdparty/paymentgateway/TicketPaymentService.js'
+import { accountIDValidator } from '../helpers/validators.js';
 export default class TicketService {
   /**
    * Should only have private methods other than the one below.
@@ -9,6 +10,10 @@ export default class TicketService {
   purchaseTickets (accountId, ...ticketTypeRequests) {
     const ticketPaymentService = new TicketPaymentService();
     
+    const accountIDErrors = accountIDValidator(accountId)
+    if(accountIDErrors.length > 0 ) {
+      throw new Error(`${accountIDErrors.join(', ')}`)
+    }
     ticketPaymentService.makePayment(accountId, 0)
     // throws InvalidPurchaseException
   }
--- test/lib/services/TicketService.spec.js
+++ test/lib/services/TicketService.spec.js
@@ -1,4 +1,6 @@
 import * as sinon from 'sinon';
+import { assert } from 'chai';
+
 import TicketPaymentService from '../../../src/thirdparty/paymentgateway/TicketPaymentService.js';
 import TicketService from '../../../src/lib/services/TicketService.js';
 
@@ -19,6 +21,11 @@ describe('TicketService', () => {
         it('should call makePayment method of ticketPaymentService', () => {
             ticketService.purchaseTickets(1, 0);
             sinon.assert.calledOnceWithExactly(makePaymentStub, 1, 0);
+        });
+
+        it('should throw an error if accountID is invalid', () => {
+            assert.throws(() => {ticketService.purchaseTickets(0, 0)}, 'AccountID cannot be zero!');
+            
         });
     });
 });
