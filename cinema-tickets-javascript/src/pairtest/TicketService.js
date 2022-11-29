f54716c7aa01915a54f7c474c7cdd4c482bbcece
Hiroji <joegilbertdev@outlook.com>
2022-11-29 11:51:11 +0000

    4-make-payments- removed unesscessary comments

--- cinema-tickets-javascript/src/pairtest/TicketService.js
+++ cinema-tickets-javascript/src/pairtest/TicketService.js
@@ -3,29 +3,6 @@ import InvalidPurchaseException from "./lib/InvalidPurchaseException.js";
 import SeatReservationService from "../thirdparty/seatbooking/SeatReservationService.js";
 import TicketPaymentService from "../thirdparty/paymentgateway/TicketPaymentService.js";
 
-/*
-Provide a working implementation of a `TicketService` that:
-- Considers the above objective, business rules, constraints & assumptions.
-
-- Calculates the correct amount for the requested tickets and makes a payment request to the `TicketPaymentService`.  
-- must be perfect request as tickets are purchased
-
-- Calculates the correct no of seats to reserve and makes a seat reservation request to the `SeatReservationService`.
-- must be perfect request as seats are booked  
-
-- Rejects any invalid ticket purchase requests. It is up to you to identify what should be deemed as an invalid purchase request.
-
-//ticketTypeRequest ARG = object {ADULT: NUM, CHILD, NUM, INFANT: NUM}
-
-# Predefined functions
-
-TicketPaymentService.makePayment(accountID: number, totalCostToPay: Number/interger)
-SeatReservationService.reserveSeat(accountId: integer, totalSeatsToAllocate: integer)
-
-CLASS TicketTypeRequest (ticketType: string, noOfTickets: number)
-CLASS InvalidPurchaseExceptions extends Errors define custom errors
-*/
-
 export default class TicketService {
   /**
    * Should only have private methods other than the one below.
