5012056cfd39837c76d140e513d090edf6363424
Oscar <osctec@hotmail.co.uk>
2023-05-13 11:54:58 +0100

    Remove java and python versions

--- cinema-tickets-java/src/main/java/thirdparty/paymentgateway/TicketPaymentService.java
+++
@@ -1,7 +0,0 @@
-package thirdparty.paymentgateway;
-
-public interface TicketPaymentService {
-
-    void makePayment(long accountId, int totalAmountToPay);
-
-}
--- cinema-tickets-python/purchase_exceptions.py
+++
@@ -1,4 +0,0 @@
-
-class InvalidPurchaseException(Exception):
-
-    pass
