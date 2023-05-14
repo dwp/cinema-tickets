290585bed844c51e6e5a9a0a4919b3850115134d
Tom Baxter <thomas99baxter@gmail.com>
2023-05-14 19:16:02 +0100

    refactor: use next to handle error with express

--- src/routes/tickets.js
+++ src/routes/tickets.js
@@ -1,8 +1,12 @@
 import { ticketsHandler } from '../lib/handlers/ticketsHandler.js'
 
 export function ticketRoutes (app) {
-  app.post('/tickets', (req, res) => {
-    const reserveSeatsResponse = ticketsHandler(req.body)
-    return res.send(reserveSeatsResponse)
+  app.post('/tickets', (req, res, next) => {
+    try {
+      const reserveSeatsResponse = ticketsHandler(req.body)
+      return res.send(reserveSeatsResponse)
+    } catch (err) {
+      next(err.message)
+    }
   })
 }
